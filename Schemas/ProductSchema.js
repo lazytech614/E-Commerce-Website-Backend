35804ad3caa92164e7ab3aa72afb5dd1943982f1
lazytech614 <derupanjan2021@gmail.com>
2024-08-21 16:31:31 +0530

    add inital endpoints for products

---
+++ Schemas/ProductSchema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+export const productSchema = mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  sizes: {
+    type: [String],
+    required: true,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import multer from "multer";
 import cors from "cors";
 import { type } from "os";
 import dotenv from "dotenv";
+import { productSchema } from "./Schemas/ProductSchema.js";
+import { error } from "console";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -52,80 +54,66 @@ app.post("/upload", upload.single("product"), async (req, res) => {
 });
 
 // Schema for creating products
-const Product = mongoose.model("Product", {
-  id: {
-    type: Number,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  new_price: {
-    type: Number,
-    required: true,
-  },
-  old_price: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  sizes: {
-    type: [String],
-    required: true,
-  },
-  available: {
-    type: Boolean,
-    default: true,
-  },
-});
+const Product = mongoose.model("Product", productSchema);
 
 // Endpoint to add product to database
 app.post("/addproduct", async (req, res) => {
-  const currentLength = (await Product.find({})).length;
-  let id;
-  if (currentLength > 0) {
-    id = (await Product.find({})).splice(-1)[0].id + 1;
-  } else {
-    id = 1;
+  try {
+    const currentLength = (await Product.find({})).length;
+    let id;
+    if (currentLength > 0) {
+      id = (await Product.find({})).splice(-1)[0].id + 1;
+    } else {
+      id = 1;
+    }
+    const product = new Product({
+      id: id,
+      name: req.body.name,
+      image: req.body.image,
+      category: req.body.category,
+      new_price: req.body.new_price,
+      old_price: req.body.old_price,
+      sizes: req.body.sizes,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Product saved in the database");
+    res.status(200).json({
+      success: 1,
+      name: req.body.name,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error saving the product in database", details: err });
   }
-  const product = new Product({
-    id: id,
-    name: req.body.name,
-    image: req.body.image,
-    category: req.body.category,
-    new_price: req.body.new_price,
-    old_price: req.body.old_price,
-    sizes: req.body.sizes,
-  });
-  console.log(product);
-  await product.save();
-  console.log("Product saved in the database");
-  res.status(200).json({
-    success: 1,
-    name: req.body.name,
-  });
 });
 
 // Endpoint to delete product from database
 app.post("/removeproduct", async (req, res) => {
-  await Product.findOneAndDelete({ id: req.body.id });
-  console.log("Product removed");
-  res.status(200).json({
-    succes: 1,
-    name: req.body.name,
-  });
+  try {
+    const productToBeDeleted = await Product.findOne({ id: req.body.id });
+    if (!productToBeDeleted) {
+      throw new Error("Product not found");
+    }
+
+    await Product.deleteOne({ id: req.body.id });
+    res.status(200).json({ message: "Product removed successfully" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error deleting the product", details: err.message });
+  }
+});
+
+// Endpoint for getting all products
+app.get("/allproducts", async (req, res) => {
+  try {
+    const allProducts = await Product.find({});
+    res.status(200).send(allProducts);
+  } catch (err) {
+    res.status(500).json({ message: "Cannot get all products", details: err });
+  }
 });
 
 app.listen(port, () => {
