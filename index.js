5844454a2c639504b1722c52ada17665a16609de
lazytech614 <143270416+lazytech614@users.noreply.github.com>
2024-08-25 13:11:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['https://e-commerce-website-frontend-ten.vercel.app/'],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  }
+));
 
 // Database connection with MongoDB
 dotenv.config();
