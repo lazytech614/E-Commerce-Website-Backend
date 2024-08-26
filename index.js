8f4ceb367288b26631762fe25f02d9131895a2ba
lazytech614 <143270416+lazytech614@users.noreply.github.com>
2024-08-27 00:46:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), async (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://e-commerce-website-backend-bicr.onrender.com/images/${req.file.filename}`,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
   });
 });
 
